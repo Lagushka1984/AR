08bc5791345457fd139282fb628e3ea608f2443b
Lagushka1984 <73789466+Lagushka1984@users.noreply.github.com>
2020-11-28 23:37:31 +0300

    Update Main.cpp

--- samples/pic_load/Main.cpp
+++ samples/pic_load/Main.cpp
@@ -1,1 +1,18 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include "Main.h"
 
+int main (int argc, char *argv[]) 
+{
+  cv::Mat image=cv::imread("ar.png");
+  if(image.empty())
+  {
+  	std::cerr << "Could not load image";
+  	return 1;
+  }
+  cv::namedWindow("Image");
+  cv::imshow("Image", image);
+  cv::waitKey();
+  return 0;
+}
